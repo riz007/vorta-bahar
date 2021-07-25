9df9864e68e44a159f437561715f039e1a2ec95d
riz007 <rizwanulrudra@gmail.com>
2021-07-26 02:15:16 +0700

    debug: added console.log

--- plugins/vue-gtag.js
+++ plugins/vue-gtag.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
+console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'production') {
   Vue.use(VueGtag, {
     config: { id: process.env.GOOGLE_ANALYTICS_ID },
