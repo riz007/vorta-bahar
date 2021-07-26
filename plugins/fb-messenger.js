6ef1346bd40ebf29206e38497f71b50fc3c908ac
riz007 <rizwanulrudra@gmail.com>
2021-07-26 12:47:38 +0700

    fix: fix eslint issues

--- plugins/fb-messenger.js
+++ plugins/fb-messenger.js
@@ -1,21 +1,21 @@
+/* eslint-disable */
+
 import Vue from 'vue'
 
-const chatbox = document.getElementById('fb-customer-chat')
-chatbox.setAttribute('page_id', '105367497522198')
-chatbox.setAttribute('attribution', 'biz_inbox')
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", '105367497522198');
+chatbox.setAttribute("attribution", "biz_inbox");
 
-const vue_fb = {}
+const vueFBMessenger = {}
 
-vue_fb.install = function install(Vue, options) {
-  ;(function (d, s, id) {
-    let js
-    const fjs = d.getElementsByTagName(s)[0]
-    if (d.getElementById(id)) return
-    js = d.createElement(s)
-    js.id = id
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js'
-    fjs.parentNode.insertBefore(js, fjs)
-  })(document, 'script', 'facebook-jssdk')
+vueFBMessenger.install = function install(Vue, options) {
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 
   window.fbAsyncInit = function onSDKInit() {
     FB.init(options)
@@ -26,7 +26,7 @@ vue_fb.install = function install(Vue, options) {
   Vue.FB = undefined
 }
 
-Vue.use(vue_fb, {
+Vue.use(vueFBMessenger, {
   appId: '105367497522198',
   autoLogAppEvents: true,
   xfbml: true,
