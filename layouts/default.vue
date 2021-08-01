375e78c19395f77bb851f6d87a402ebb7970e3bc
riz007 <rizwanulrudra@gmail.com>
2021-08-01 23:18:05 +0700

    debug

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,6 +16,7 @@
 </template>
 
 <script>
+import Vue from 'vue'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
@@ -23,6 +24,24 @@ export default {
   components: {
     Navbar,
     Footer,
+  },
+  data() {
+    return {
+      isFBMessengerReady: false,
+    }
+  },
+  mounted() {
+    this.isFBMessengerReady = Vue.FB !== undefined
+    window.addEventListener('fb-messenger-ready', this.onFBMessengerReady)
+    window.FB()
+  },
+  beforeDestroy() {
+    window.removeEventListener('fb-messenger-ready', this.onFBMessengerReady)
+  },
+  methods: {
+    onFBMessengerReady() {
+      this.isFBMessengerReady = true
+    },
   },
 }
 </script>
