7c776abf2a2e221e3d83e1ffdaa84af6a9ed11c3
riz007 <rizwanulrudra@gmail.com>
2021-08-01 23:26:04 +0700

    debug

--- static/js/fb-sdk.js
+++ static/js/fb-sdk.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 var chatbox = document.getElementById('fb-customer-chat');
-console.log(Chatbox);
+console.log(chatbox);
 chatbox.setAttribute("page_id", '105367497522198');
 chatbox.setAttribute("attribution", "biz_inbox");
 
